3d0b1abad1c77662e54b665c8020fb80f8b54ffc
Michael Wydra <wydrama@gmail.com>
2021-05-07 12:38:07 -0500

    change path

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/wayfarer10/'));
+app.use(express.static('./dist/wayfarer/'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/wayfarer10/index.html'}),
+  res.sendFile('index.html', {root: 'dist/wayfarer/index.html'}),
 );
 
 // Start the app by listening on the default Heroku port
